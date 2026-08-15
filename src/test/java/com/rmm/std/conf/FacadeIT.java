94a3a00eaf6819e1fe62069a452c72518e949b92
mikaiahSTD <hei.mikaiah@gmail.com>
2026-08-15 22:00:10 +0300

    fix(test): resolve Testcontainers dependency issues

--- src/test/java/com/rmm/std/conf/FacadeIT.java
+++ src/test/java/com/rmm/std/conf/FacadeIT.java
@@ -6,13 +6,22 @@ import com.rmm.std.PojaGenerated;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @PojaGenerated
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @Slf4j
+@Testcontainers
 public class FacadeIT {
+  @Container @ServiceConnection
+  static PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:16");
 
   @SneakyThrows
   @DynamicPropertySource
